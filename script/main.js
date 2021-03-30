2738335420a7e354110ab91fd203c651e4d1478c
Anlqn <lequangngocan@gmail.com>
2021-03-30 17:01:13 +0700

    anlqn update code

--- script/main.js
+++ script/main.js
@@ -1,9 +1,9 @@
 // slide
 function showSlide(n) {
-    var items = document.getElementsByClassName('item');
-    var dots = document.getElementsByClassName('dot');
-    var prev = document.getElementById('nav-prev');
-    var next = document.getElementById('nav-next');
+    var items = document.querySelectorAll('.item');
+    var dots = document.querySelectorAll('.dot');
+    var prev = document.querySelectorAll('.button-prev')[0];
+    var next = document.querySelectorAll('.button-next')[0];
 
     if(n >= 0 && n < items.length) {
 
@@ -25,26 +25,53 @@ function showSlide(n) {
             showSlide(n+1);
         };
     }
+    
 }
 showSlide(0);
 
+var slideIndex = 0;
+autoSlide();
+
+function autoSlide() {
+    
+    var items = document.querySelectorAll('.item');
+    var dots = document.querySelectorAll('.dot');
+    
+    for(var i = 0; i < items.length; i++) {
+        items[i].style.display = 'none';
+    }
+
+    slideIndex++;
+    if(slideIndex >= items.length) {
+        slideIndex = 0;
+    }
+    items[slideIndex].style.display = 'block';
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(' active', '');
+    }
+    dots[slideIndex].className += ' active';
+
+    setTimeout(autoSlide, 2000);
+}
+
 // toggle menu
 function toggleMenu() {
-    var menu = document.getElementById('menu-mobile');
-    var active = menu.getAttribute('class').split(' ');
+    var menu = document.querySelectorAll('.menu-mobile');
+    var active = menu[0].getAttribute('class').split(' ');
     for(var i = 0; i < active.length; i++) {
         if(active[i] == 'active') {
-            document.getElementById('menu-mobile').classList.remove("active");
+            menu[0].classList.remove("active");
         } else {
-            document.getElementById('menu-mobile').className += ' active';
+            menu[0].className += ' active';
         }
     }
 }
 
 // carousel 
 function carousel(n) {
-    var items = document.getElementsByClassName('carousel');
-    var dotted = document.getElementsByClassName('dotted');
+    var items = document.querySelectorAll('.carousel');
+    var dotted = document.querySelectorAll('.dotted');
 
     if(n >= 0 && n < items.length) {
 
